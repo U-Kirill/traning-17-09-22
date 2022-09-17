1391294e4874a9ff738188d52af3728a1873df00
Kirill <kglad67@gmail.com>
2022-09-17 11:11:19 +0300

    add enemy health

---
+++ Assets/Scripts/Logic/Enemy/EnemyHealth.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+namespace Logic.Enemy
+{
+    public class EnemyHealth : MonoBehaviour
+    {
+        [SerializeField] private int _max = 100;
+
+        public event Action Damaged;
+        public event Action Healed;
+        
+        public int Max => _max;
+
+        public int Current { get; private set; }
+
+        private void Start()
+        {
+            MakeFull();
+        }
+
+        public void MakeFull()
+        {
+            Current = Max;
+        }
+
+        public void Damage(int amount)
+        {
+            if (amount < 0)
+                throw new ArgumentOutOfRangeException(nameof(amount));
+
+            if (Current == 0)
+                return;
+            
+            int overDamage = Mathf.Max(amount - Current, 0);
+            int damage = amount - overDamage;
+            Current -= damage;
+
+            Damaged?.Invoke();
+        }
+        
+        public void Heal(int amount)
+        {
+            if (amount < 0)
+                throw new ArgumentOutOfRangeException(nameof(amount));
+
+            if (Current == Max)
+                return;
+
+            int maxPossibleHeal = Max - Current;
+            int overHeal = Mathf.Max(amount - maxPossibleHeal, 0);
+            int heal = amount - overHeal;
+            Current += heal;
+
+            Healed?.Invoke();
+        }
+    }
+}
