5c43ac4d4b954f4f7d5d4a62d5c22dc887bceafa
Kirill <kglad67@gmail.com>
2022-09-17 10:49:05 +0300

    player move

---
+++ Assets/Scripts/Logic/PlayerMove.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Services;
+using UnityEngine;
+using Zenject;
+
+namespace Logic
+{
+    public class PlayerMove : MonoBehaviour
+    {
+        [SerializeField] private CharacterController _controller;
+        [SerializeField] private float _speed = 5;
+
+        private IInputService _input;
+
+        [Inject]
+        public void Constructor(IInputService input)
+        {
+            _input = input;
+        }
+        
+        private void Update()
+        {
+            Vector3 movement = new Vector3(_input.Value.x, 0, _input.Value.y);
+            _controller.Move(movement * _speed * Time.deltaTime);
+        }
+    }
+}
